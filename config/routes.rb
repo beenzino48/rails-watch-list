e253482e596f884ae76128bc5fd4dfe872dead31
hamish liu <hamish.mc.liu@gmail.com>
2022-07-14 14:33:53 +0900

    tried taking code out of the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # root to set the base_url
-  root to: 'lists#index'
+
   # The list needs to show an individual list, all lists, create  a list and destroy
   # bookmarks are made within lists so they need to be nested
   resources :lists, only: [:index, :show, :create, :new, :destroy] do
